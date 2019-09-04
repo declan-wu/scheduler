68fe96cbf6e990098139178e736c5dfc436a9248
Declan Wu <declan.s.wu@gmail.com>
2019-09-04 14:46:04 -0400

    added more custom hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,8 +2,24 @@ import React, { useState } from "react";
 
 const useVisualMode = initial => {
   const [mode, setMode] = useState(initial);
-  const transition = newState => setMode(newState);
-  return { mode, transition };
+  const [history, setHistory] = useState([initial]);
+
+  const transition = newState => {
+    setHistory(preState => {
+      preState.push(newState);
+      console.log(preState);
+      return preState;
+    });
+    setMode(newState);
+  };
+  const back = () => {
+    setHistory(preState => {
+      preState.pop();
+      setMode(preState[preState.length - 1]);
+      return preState;
+    });
+  };
+  return { mode, transition, back };
 };
 
 export default useVisualMode;
